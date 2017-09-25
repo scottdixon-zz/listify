7c6c281d350971dc5dea36a75697458a2d1839fb
Scott Dixon <scott@howrefreshing.com.au>
2017-09-25 16:46:18 +1000

    Listify + tests

---
+++ listify.rb
@@ -0,0 +1,16 @@
+class Listify
+	attr_accessor :list
+	def initialize
+		@list = []
+	end
+	def add_item (title, qty)
+		@list.push({title: title, qty: qty})
+	end
+	def pretty_list
+		pretty_list = ''
+		@list.each do |item|
+			pretty_list += "#{item[:qty]} x #{item[:title]}\n"
+		end
+		return pretty_list
+	end
+end
---
+++ listify_test.rb
@@ -0,0 +1,22 @@
+require 'test/unit'
+require_relative 'listify'
+
+class ListifyTest < Test::Unit::TestCase
+	def test_empty_list
+		shopping = Listify.new
+		assert_equal(shopping.list, [])
+	end
+	def test_add_item
+		shopping = Listify.new
+		shopping.add_item("Coke", 2)
+		assert_equal(shopping.list.size, 1)
+	end
+	def test_pretty_list
+		shopping = Listify.new
+		shopping.add_item("Milk", 2)
+		assert_equal(shopping.pretty_list, "2 x Milk\n");
+	end
+	def test_replace_coke_with_water
+		# Write your first test here
+	end
+end
